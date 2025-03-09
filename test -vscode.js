cb35a97ff4307d758420aa48d24462460d265e90
Nishtha Dhiman 👩‍💻 <mytrendytreasure9@gmail.com>
2025-03-09 12:28:09 +0530

    Add files via upload

---
+++ test -vscode.js
@@ -0,0 +1,9 @@
+const vscode = require('vscode');
+
+function activate(context) {
+    console.log('Congratulations! Your extension "girdhar-extension" is now active!');
+}
+
+module.exports = {
+    activate
+};
